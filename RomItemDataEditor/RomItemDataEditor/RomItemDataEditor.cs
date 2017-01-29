c54f109083d01b5575620a3d27380dc2a4852145
Auke Tegel <aukieshomebrew@gmail.com>
2017-01-29 21:01:13 +0100

    0.1 - Added usage/help text.

--- RomItemDataEditor/RomItemDataEditor/RomItemDataEditor.cs
+++ RomItemDataEditor/RomItemDataEditor/RomItemDataEditor.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿
+
+using System;
 using System.IO;
 using CommandLine;
+using CommandLine.Text;
 
 namespace RomItemDataEditor
 {
@@ -21,7 +24,7 @@ namespace RomItemDataEditor
 
             if (!cmd.ParseArguments(args, opt))
             {
-                Console.WriteLine("Couldn't parse arguments.");
+               Console.WriteLine( opt.GetUsage());
                 return 1;
             }
             else
@@ -43,23 +46,24 @@ namespace RomItemDataEditor
 
                 RomReader rr = new RomReader(opt.ArgRomPath, opt.ArgDataPath);
 
-                Console.WriteLine("Game: {0}", rr.GetGameName());
                 
-                if(opt.ArgStructName == "")
-                {
-                    Console.WriteLine("{1}> {0}", rr.GetItemNameByIndex(opt.ArgIndex), "name");
 
+                po.PrintStrValue(rr.GetGameName(), "Game");
+                
+                if(opt.ArgGetName)
+                {
+                    po.PrintStrValue(rr.GetItemNameByIndex(opt.ArgIndex), "Name");
                 }
                 else
                 {
                     
                     if (opt.ArgHexPrint)
                     {
-                        Console.WriteLine("{1}> 0x{0:X}", rr.GetItemStructValueByIndex(opt.ArgIndex, opt.ArgStructName), opt.ArgStructName);
+                        po.PrintHexValue(rr.GetItemStructValueByIndex(opt.ArgIndex, opt.ArgStructName), opt.ArgStructName);
                     }
                     else
                     {
-                        Console.WriteLine("{1}> {0}", rr.GetItemStructValueByIndex(opt.ArgIndex, opt.ArgStructName), opt.ArgStructName);
+                        po.PrintDecValue(rr.GetItemStructValueByIndex(opt.ArgIndex, opt.ArgStructName), opt.ArgStructName);
                     }
                 }
 
@@ -69,8 +73,28 @@ namespace RomItemDataEditor
 
             }
 
-           
-         
+
+        }
+
+
+        public void PrintHexValue(int val, string name)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("{1}: 0x{0:X}", val, name);
+            Console.ResetColor();
+        }
+        public void PrintDecValue(int val, string name)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("{1}: {0}", val, name);
+            Console.ResetColor();
+        }
+
+        public void PrintStrValue(string val, string name)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("{1}: {0}", val, name);
+            Console.ResetColor();
         }
 
 
@@ -82,17 +106,36 @@ namespace RomItemDataEditor
         [Option('f', "rom-file", Required = true, HelpText = "Path of the GBA ROM.")]
         public string ArgRomPath { get; set; }
 
-        [Option('m', "xml-file", Required = false, HelpText = "Path of the XML file.", DefaultValue ="data.xml")]
+        [Option('x', "xml-file", Required = false, HelpText = "Path of the XML file.", DefaultValue ="data.xml")]
         public string ArgDataPath { get; set; }
 
-        [Option('i', "index-number", Required = false, HelpText = "Index of the item.", DefaultValue = 0)]
+        [Option('i', "index-number", Required = true, HelpText = "Index of the item.", DefaultValue = 0)]
         public int ArgIndex { get; set; }
 
-        [Option('n', "data-name", Required = false, HelpText = "Struct name to get value.", DefaultValue = "")]
+        [Option('d', "data-name", Required = false, HelpText = "Struct name to get value from.")]
         public string ArgStructName { get; set; }
 
-        [Option('x', "print-hex", Required = false, HelpText = "Print hexidecimal value.")]
+        [Option("print-hex", Required = false, HelpText = "Print hexidecimal value.")]
         public bool ArgHexPrint { get; set; }
+
+        [Option('n', "get-name", Required = false, HelpText = "Get item name.")]
+        public bool ArgGetName { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            HelpText help = new HelpText
+            {
+                Heading = new HeadingInfo("romitemdataeditor", "0.1"),
+                Copyright = new CopyrightInfo("Aukie's Homebrew", 2017),
+                AdditionalNewLineAfterOption = true,
+                AddDashesToOption = true
+
+            };
+            help.AddPreOptionsLine("Usage: romitemdataeditor --rom-file <*.gba file> [--data-file <*.xml file>] [--index-number <index number>] [--data-name <datamember name>] [--print-hex]");
+            help.AddOptions(this);
+            return help;
+        }
 
     }
 }
